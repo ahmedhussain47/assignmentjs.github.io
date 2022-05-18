647041c64c5d897800169d63208dad769319c976
ahmedhussain47 <68425053+ahmedhussain47@users.noreply.github.com>
2022-05-18 19:39:06 +0500

    jsassignment

---
+++ app.js
@@ -0,0 +1,185 @@
+var age = 22;
+var agef = "I am "+age+" years old";
+//alert(agef);
+
+var count = 04;
+var countf = "You have visited this site "+count+" times";
+//alert(countf);
+
+// var byear = 1998;
+// var byearf = "<h1>" +"My birth year is "+byear+ "</h1> \n<h2>Data Type of Variable is integer </h2><h3>\nReload The Page</h3>";
+
+// var Visitor = "John Doe ";
+// var Quantity = 5;
+// var product = " T-Shirts";
+// var Customer = "<h1>" +Visitor+ "ordered "+ Quantity+ product+"</h1><h3>\nReload The Page</h3>";
+
+
+
+
+
+// Chapter 4
+var num1, num2, num3;
+num1=10;
+num1=20;
+num1=30;
+
+// Q2
+
+// var legalvar1;
+// var legalVar2;
+// var $legalvar3;
+// var _legalvar4;
+// var Legal_Var5;
+
+// var 1illegalvar;
+// var ille~galvar;
+// var var;
+// var ill-egalvar;
+// var 1illega]lvar;
+
+// Q3
+// done in index.html page using script, code is also here
+// document.write("“Rules for naming JS variables”");
+// document.write("Variable names can only contain Numbers, Alphabets, Special characters and underscores. For example $my_1stVariable");
+// document.write("Variables must begin with a Special character, Alphabets or underscores. For example $name, _name or name");
+// document.write("Variable names should not be JS Keywords");
+
+
+
+
+// chapter 5     
+
+// var numb1 = 3;
+// var numb2 = 5;
+// ans1 = numb1 + numb2;ans2=numb1-numb2;
+// ans3=numb1*numb2;ans4=numb1/numb2;
+// ans5=numb1+numb2;
+// document.write("***Question-1"+"<br>"); 
+// document.write("The sum of "+numb1+ " and " +numb2+ " = "+ans1+"<br>"); 
+// document.write("<br>"+"***Question-2"+"<br>"); 
+// document.write("The subtraction of "+numb1+ " and " +numb2+ " = "+ans2+"<br>"); 
+// document.write("The multiplication of "+numb1+ " and " +numb2+ " = "+ans3+"<br>"); 
+// document.write("The division of "+numb1+ " and " +numb2+ " = "+ans4+"<br>"); 
+// document.write("The mode of "+numb1+ " and " +numb2+ " = "+ans5+"<br>"); 
+
+// document.write("<br>"+"***Question-3"+"<br>"); 
+
+// document.write("<br>"+"Value after variable declaration is: "+val+"<br>"); 
+// var val = 5;
+
+// document.write("Value after increment is: "+ ++val +"<br>"); 
+// var val2 = 7;
+// answer = val + val2;
+// document.write("Value after addition is: "+answer +"<br>"); 
+// document.write("Value after deccrement is: "+ --answer +"<br>");
+// aswer= answer % 3;
+// document.write("Value after increment is: "+ aswer +"<br>");
+
+// document.write("<br>"+"***Question-4"+"<br>"); 
+// var tic = 600;
+// var ticnum = 5;
+// document.write("Total cost to buy "+ ticnum + "tickets is "+tic*ticnum+"-PKR"+"<br>");
+
+// document.write("<br>"+"***Question-5"+"<br>"); 
+// var tabnum = 4;
+// document.write("Table of "+ tabnum +"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*1+"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*2+"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*3+"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*4+"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*5+"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*6+"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*7+"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*8+"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*9+"<br>");
+// document.write(tabnum +" x "+" = "+tabnum*10+"<br>");
+
+// document.write("<br>"+"***Question-6"+"<br>");
+// var tmp = 25;
+// var ftemp = 70;
+// tmp = (25 * 9)/5 + 32;
+// ftmp =(ftemp-32)*5/9;
+// document.write("25C is: "+ tmp +"F"+"<br>");
+// document.write(ftemp+"F is: "+ ftmp +"C"+"<br>");
+
+// document.write("<br>"+"***Question-7"+"<br>");
+// var item1 = 650;
+// var it1quant = 3;
+// it1 = item1 * it1quant;
+// var item2 = 100;
+// var it2quant = 7;
+// it2 = item2 * it2quant;
+// var charges = 100;
+// orderbill = it1 + it2 + charges;
+// document.write("Price of item 1: "+ item1 +"<br>");
+// document.write("Quantity of item 1: "+ it1quant +"<br>");
+// document.write("Price of item 2: "+ item2 +"<br>");
+// document.write("Quantity of item 2: "+ it2quant +"<br>");
+// document.write("Shipping charges: "+ charges +"<br>");
+// document.write("<br>"+"Total cost of your order: "+ orderbill +"<br>");
+
+// document.write("<br>"+"***Question-8"+"<br>");
+// var totalmarks = 980;
+// var mymarks = 804;
+// percentage= (100 * mymarks) / totalmarks;
+// document.write("Percentage is: "+ percentage +"<br>");
+
+
+// document.write("<br>"+"***Question-9"+"<br>");
+// var dol =10;
+// tdol = dol * 104.80;
+// var rial = 25;
+// trial = rial * 28;
+
+// totalc = trial+tdol;
+// document.write("Total Currency in PKR is: "+ totalc +"<br>");
+
+
+// document.write("<br>"+"***Question-10"+"<br>");
+// var btyr = 1992;
+// var cyear =2016;
+// yrage = cyear - btyr;
+// document.write("Current year is: "+ cyear +"<br>");
+// document.write("Birth year is: "+ btyr +"<br>");
+// document.write("you are "+yrage  +" years old"+"<br>");
+
+
+// chapter 6
+// document.write("***Question-1"+"<br>");    
+//             document.write("Result");    
+//             var vall = 10;
+//             document.write("The value of a is: "+vall+"<br>");   
+//             document.write("******************"+"<br><br>");    
+ 
+//             vall++
+//             document.write("The value of ++a is: "+vall+"<br>");    
+//             document.write("Now The value of ++a is: "+vall+"<br><br>");  
+
+//             document.write("The value of a++ is: "+vall+"<br>");  
+//             vall++
+//             document.write("The value of a++ is: "+vall+"<br><br>");  
+
+//             --vall
+//             document.write("The value of --a is: "+vall+"<br>");  
+//             document.write("The value of a++ is: "+vall+"<br><br>");  
+
+//             document.write("The value of a-- is: "+vall+"<br>");  
+//             vall--
+//             document.write("The value of a-- is: "+vall+"<br><br>");  
+
+
+//             document.write("***Question-2"+"<br>");    
+//             var a = 2, b = 1;
+//             document.write("value of a is "+a+"<br>");    
+//             document.write("value of b is "+b+"<br>");
+//             var result = --a - --b + ++b + b--;
+//             document.write("Result is "+result+"<br>"); 
+//             --a
+//             document.write("Result of --a is "+a+"<br>"); 
+//             var result1 = --a - --b;
+//             document.write("Result is "+result1+"<br>");  
+//             var result2 = --a - --b + ++b;;
+//             document.write("Result is "+result2+"<br>");  
+//             var result3 = --a - --b + ++b + b--;
+//             document.write("Result is "+result3+"<br>");   
